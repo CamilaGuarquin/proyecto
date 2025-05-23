3eba3a159b9a76bfa4d8f6d8800e67f4bf7b6576
Camila Guarquin <camiguarquinruiz@gmail.com>
2025-05-23 18:54:56 -0500

    finalizamos el tutorial de apoyo

--- main.py
+++ main.py
@@ -81,8 +81,9 @@ def chatbot(query: str):
         "peliculas": results
     })
     
-# Ruta para buscar películas por categoría específica
+    # Ruta para buscar películas por categoría específica
+
 @app.get ('/movies/', tags=['Movies'])
 def get_movies_by_category(category: str):
     # Filtramos la lista de películas según la categoría ingresada
-    return [m for m in movies_list if category.lower() in m ['category'].lower]
+    return [m for m in movies_list if category.lower() in m ['category'].lower()]

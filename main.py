e2cb9fef67bbed6345fc387196feaaa10f4b4197
Camila Guarquin <camiguarquinruiz@gmail.com>
2025-05-23 19:35:08 -0500

    Finalizamos el tutorial de apoyo

--- main.py
+++ main.py
@@ -1,34 +1,35 @@
-""" 
-Imagina que esta API es una biblioteca de peliculas:
-La funcion load_movies() es como una biblioteca que carga el catalogo de libros (peliculas) cuando se abre la biblioteca.
-La funcion get_movies() muestra todo el catalogo cuando alguien lo pide.
-La funcion get_movie(id) es como si alguien preguntara por un libro especifico es decir, por un coidgo de identificacion.
-La funcion chatbot (query) es un asistente que busca peliculas segun palabras clave y sinonimo.
-La funcion get_movies_by_category(category) ayuda a encontrar peliculas segun su genero (accion, comedia, etc...)
-"""
+''' 
+imagina que esta API es una biblioteca de peliculas
+La función load_movies() es como una biblioteca de libros (peliculas) cuando se abre la biblioteca
+La función get_movies() muestra todo el catalogo cuando alguien lo pide.
+La fucnción get_movie(id) es como si alguien preguntara por un libro especifico es decir, por un codigo de identificación
+La funcion chat bot (query) es un asistente que busca peliculas segun palabras clave y sinonimo.
+La función get_movies_by_category(category) ayuda a encontrar peliculas segun su genero (acción, comedia, etc...)
+'''
+
 # Importamos las herramientas necesarias para continuar nuestra API
-from fastapi import FastAPI, HTTPException # FastAPI nos ayuda a crear la API, HTTPException nos ayuda a manejar errores  # noqa: F401
-from fastapi.responses import HTMLResponse, JSONResponse # HTMLResponse nos ayuda a manejar respuestas HTML, JSONResponse nos ayuda a manejar respuestas en formato JSON
-import pandas as pd # pandas nos ayuda a manejar datos en tablas como si fuera un Excel
+from fastapi import FastAPI, HTTPException # FastAPI nos ayuda a crear la API, HTTPException nos ayuda a mejorar errores
+from fastapi.responses import HTMLResponse, JSONResponse # HTMLResponse nos ayuda a mejorar respuestas HTML, JSONResponse nos ayuda a manejar respuestas
+import pandas as pd # pandas nos ayuda a manejar tablas como si fuera un Excel
 import nltk # nltk es una libreria para procesar texto y analizar palabras
 from nltk.tokenize import word_tokenize # word_tokenize nos ayuda a tokenizar texto, es decir, a convertirlo en palabras
 from nltk.corpus import wordnet # wordnet es una libreria para analizar sinonimos
 
-# indicamos la ruta donde nltk buscara los datos descargados en nuestro computador
-nltk.data.path.append(r'C:\Users\CAMILA\AppData\Roaming\nltk_data\tokenizers')
-nltk.download('punkt') # Paquete para dividir frases en palabras
-nltk.download('wordnet') # Paquete para encontrar sinonimos en palabras
+# Indicamos la ruta donde nltk buscara los datos descargados en nuestro computador
+nltk.data.path.append(r'C:\Users\USER\AppData\Roaming\nltk_data')
+nltk.download('punkt') # es un paquete para dividir frases en palabras
+nltk.download('punkt_tab') # paquete para encontrar sinonimos en palabras
 
-# Función para cargar las peliculas  desde un archivo csv
+# función para cargar las peliculas desde un archivo csv
 
 def load_movies():
-    #Leemos el archivo qur contiene información de peliculas y seleccioanmos las columnas mas importantes
-    df = pd.read_csv(r"./Dataset/netflix_titles.csv")[['show_id','title','release_year','listed_in', 'rating','description']]
+    # leemos el archivo que contiene información de peliculas y selecciónamos las columnas mas importantes 
+    df = pd.read_csv("Dataset/netflix_titles.csv")[['show_id','title','release_year','listed_in','rating','description']]
     
-    # Renombramos las columnas para que sean más fáciles de entender
-    df.columns = ['id', 'year', 'category', 'genres', 'rating', 'overview']
+    # Renombramos las colunmas para que sean mas faciles de entender
+    df.columns = ['id', 'title', 'year', 'category', 'rating', 'overview']
     
-    # LLenamos los espacios vacios con texto vacio y convertimos los datos en una lista de diccionaruis 
+    # Llenamos los espacios vacios con texto vacio y convertimos los datos en una lista de diccionarios
     return df.fillna('').to_dict(orient='records')
 
 # Cargamos las peliculas al iniciar la API para no leer el archivo cada vez que alguien pregunte por ellas
@@ -37,53 +38,50 @@ movies_list = load_movies()
 # Función para encontrar sinonimos de una palabra
 def get_synonyms(word):
     # Usamos wordnet para encontrar distintas palabras que significan lo mismo
-    return{lemma.name().lower() for syn in wordnet.synsets_(word) for lemma in syn.lemmas()}
+    return{lemma.name().lower() for syn in wordnet.synsets(word) for lemma in syn.lemmas()}
 
-# Creamos la aplicación FastAPI, que sera el motot de nuestro API
+# Creamos la aplicación FastAPI, que será el motor de nuestra API
 # Esto inicializa la API con una versión
-
-app = FastAPI(tittle='Mi aplicación de películas', version='1.0.0')
+app = FastAPI(title='mi aplicacion de peliculas', version='1.0.0')
 
 @app.get('/', tags=['Home'])
 def home():
-    # Cuando entremps en el navegador a a http://127.0.0.1:8000 veremos un mensaje de bienvenida
-    return HTMLResponse('<h1> Bienvenido a la API de peliculas </h1>') 
-
-# Obteniendo la lista de peliculasq
-# Creamos una ruta para obtener todas las peliculas
-# Ruta para obtener todas las peliculas 
+    # cuando entremos en el navegador a http://127.0.0.1:8000 veremos un mensaje de bienvenida
+    return HTMLResponse('<h1> Bienvenido a la API de peliculas </h1>')
+    
+# Obteniendo la lista de películas
+# Creamos una ruta para obtener todas las películas
+# Ruta para obtener todas las películas
 @app.get('/movies', tags=['Movies'])
 def get_movies():
-    # Si hay peliculas, las enviamos, si no mostramos un error
-    return movies_list or HTMLResponse(status_code=500, detail="No hay datos de peliculas disponibles")
-
-# Ruta para obtener una pelicula específica por su ID
+    # Si hay películas, las enviamos, si no mostramos un error
+    return movies_list or HTMLResponse(status_code=500, detail="No hay datos de películas disponibles")
+# Ruta para obtener una película específica por su ID
+# ruta para obtener una película específica por su ID
 @app.get('/movies/{id}', tags=['Movies'])
 def get_movies(id: str):
-    # Buscamos en la lista de peliculas la que tenga el mismo ID
-    return next((m for m in movies_list if m ['id'] == id), {"Detalle": "Pelicula no encontrada"})
-
-# Ruta del chatbot que responde a películas según palabras clave de la categoria
+     # buscamos en la lista de películas la que tenga el mismo ID
+     return next((m for m in movies_list if m ['id'] == id), {"detalle": "película no encontrada"})
+# Ruta del chatbot que responde con películas segun palabras clave de la categoria
 @app.get('/chatbot', tags=['chatbot'])
 def chatbot(query: str):
-    # Dividimos la consulta en palbras clave, para entener mejor la intensión del usuario
+    # Dividimos la consulta en palabras clave, para entender mejor la intension del usuario
     query_words = word_tokenize(query.lower())
-    # Buscamos sinónimos de las palabras clave para ampliar la búsqueda
+    # Buscamos sinonimos de las palabras clave para ampliar la busqueda
     synonyms = {word for q in query_words for word in get_synonyms(q)} | set(query_words)
     
     # Filtramos la lista de peliculas buscando coinsidencias en la categoria
-    results =[m for m in movies_list if any (s in m ['category'].lowe() for s in synonyms)]
+    results =[m for m in movies_list if any (s in m ['category'].lower() for s in synonyms)]
     
-    # Si encontramos las peliculas, enviamos la lista de peliculas; sino, mostramos un mensaje de que no se encontaron coinsidencias 
+    # Si encontramos las peliculas, enviamos la lista de películas; sino, ostramos un mensaje de que no se encontraron cinsidencias
     
     return JSONResponse (content={
-        "respuesta": "aqui tienes algunas peliculas relacionadas." if results else "no encontre peliculas en esa categoria.", 
-        "peliculas": results
+    "respuesta": "aqui tienes algunas peliculas relacionadas." if results else "no encontre peliculas en esa categoria.",
+    "peliculas": results
     })
     
-    # Ruta para buscar películas por categoría específica
-
-@app.get ('/movies/', tags=['Movies'])
+# Ruta para buscar películas por categoría específica
+@app.get ('/movies/by_category/', tags=['Movies'])
 def get_movies_by_category(category: str):
     # Filtramos la lista de películas según la categoría ingresada
     return [m for m in movies_list if category.lower() in m ['category'].lower()]
